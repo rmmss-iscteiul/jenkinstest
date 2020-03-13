2c40522ba1419639d14339376ad8b14dacaf4387
RodrigoSantos <rmmss@iscte-iul.pt>
2020-03-13 16:35:22 +0000

    aa

---
+++ jenkinstest/src/main/java/jenkinstest/jenkinstest/App.java
@@ -0,0 +1,13 @@
+package jenkinstest.jenkinstest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jenkinstest/src/main/java/jenkinstest/jenkinstest/test.java
@@ -0,0 +1,7 @@
+package jenkinstest.jenkinstest;
+
+public class test {
+	public static int metodo() {
+		return 1;
+	}
+}
